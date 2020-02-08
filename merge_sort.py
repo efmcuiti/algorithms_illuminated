13fa9838990e516cae9e867d168efd437636b4c6
Edison Mendez <efmcuiti@gmail.com>
2020-02-07 19:35:32 -0500

    Plus count inversions

--- merge_sort.py
+++ merge_sort.py
@@ -9,13 +9,13 @@ def mergeSort(l):
     Return the same list but ordered.
     """
     assert type(l) is list
-
     if not l or len(l) is 1:
-        return l
+        return (0, l)
     else:
         half = len(l) // 2
         lh, rh = mergeSort(l[:half]), mergeSort(l[half:])
-        return merge(lh, rh)
+        m = merge(lh[1], rh[1])
+        return (lh[0] + rh[0] + m[0], m[1])
 
 def merge(l1, l2):
     """
@@ -25,7 +25,7 @@ def merge(l1, l2):
     """
     n = len(l1) + len(l2)
     merged = []
-    i, j, = 0, 0
+    i, j, s = 0, 0, 0
     while i < len(l1) and j < len(l2):
         if l1[i] < l2[j]:
             merged.append(l1[i])
@@ -33,10 +33,12 @@ def merge(l1, l2):
         else:
             merged.append(l2[j])
             j += 1
+            s += (n//2)-i
+
 
     complement = [e for e in l1[i:]] if i < len(l1) else [e for e in l2[j:]]
 
     assert len(complement) + len(merged) is n
 
-    return merged + complement
+    return (s, merged + complement)
 
