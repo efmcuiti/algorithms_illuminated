334ac045ff44e7a7b04c9744aa560bca1d4bc66a
Edison Mendez <efmcuiti@users.noreply.github.com>
2020-05-06 14:21:04 -0400

    Finish the brute force algorithm

--- matrix_multip.py
+++ matrix_multip.py
@@ -1,7 +1,19 @@
 """ 
-	@author efmcuiti
-	Testing file for algorithms.
+Created on: May 5, 2020
+Author efmcuiti
+Testing file for algorithms.
 """
-def direct_multiplication(matrixA, matrixB):
-	'''
-	'''
+def directMultiplication(A, B):
+	"""
+	A, B: Matrices of size nxn.
+	
+	Returns the matrix resulting on the brute force multiplication AxB.
+	"""
+	assert len(A) == len(B)
+	Z = [[None for j in range(len(A))] for i in range(len(A))]
+	for i in len(A):
+		for j in len(A):
+			Z[i][j] = 0
+			for k in len(A):
+				Z[i][j] += A[i][k] * B[k][j]
+	return Z
