bcb359d7f13aaee3be4f409177044aec98108562
Edison Mendez <efmcuiti@users.noreply.github.com>
2020-05-06 21:40:40 -0400

    complete tested brute force multiplication algorithm

--- matrix_multip.py
+++ matrix_multip.py
@@ -10,10 +10,19 @@ def directMultiplication(A, B):
 	Returns the matrix resulting on the brute force multiplication AxB.
 	"""
 	assert len(A) == len(B)
-	Z = [[None for j in range(len(A))] for i in range(len(A))]
-	for i in len(A):
-		for j in len(A):
+	n = len(A)
+	Z = [[None for j in range(n)] for i in range(n)]
+	for i in range(n):
+		for j in range(n):
 			Z[i][j] = 0
-			for k in len(A):
+			for k in range(n):
 				Z[i][j] += A[i][k] * B[k][j]
 	return Z
+
+# #Ussage:
+# **brute force**:
+# 1. Define the matrices:
+# x = [[2, 1],[3, 4]]
+# y = [[5, 6],[7, 8]]
+# r = directMultiplication(x, y)
+# print(r)
